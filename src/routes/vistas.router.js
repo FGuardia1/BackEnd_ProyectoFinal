37ac1fb08d0db75a0414deb4a5f1244b43c4af71
Fernando Guardia <fguardia321@gmail.com>
2023-03-28 23:39:22 -0300

    arreglo bug chat

--- src/routes/vistas.router.js
+++ src/routes/vistas.router.js
@@ -9,7 +9,9 @@ import { proyectConfig } from "../../utils/configs/config.js";
 routerViews.get("/home", isValidAuthToken, renderHome);
 
 routerViews.get("/chat", isValidAuthToken, (req, res, next) => {
-  let admin = true;
+  let admin;
+  if (req.user.rol == "admin") admin = true;
+  else admin = false;
   res.render("chat", { email: req.user.email, admin });
 });
 
